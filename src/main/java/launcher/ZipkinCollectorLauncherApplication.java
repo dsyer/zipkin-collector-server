4ca5c20c764d4d7f717916929ddae8fe527d8b6a
Dave Syer <dsyer@pivotal.io>
2015-09-22 15:15:45 +0100

    Extract createdTs from annotations

--- src/main/java/launcher/ZipkinCollectorLauncherApplication.java
+++ src/main/java/launcher/ZipkinCollectorLauncherApplication.java
@@ -26,6 +26,28 @@ public class ZipkinCollectorLauncherApplication {
 	@ServiceActivator(inputChannel = Sink.INPUT)
 	@Transactional
 	public void sink(Span span) {
+		long ts = System.currentTimeMillis();
+		for (Annotation annotation : span.getAnnotations()) {
+			this.jdbcTemplate.update(
+					"INSERT INTO zipkin_annotations (trace_id,span_id,span_name,service_name,value,ipv4,port,a_timestamp) values (?,?,?,?,?,?,?,?)",
+					(PreparedStatement ps) -> {
+						ps.setLong(1, span.getTrace_id());
+						ps.setLong(2, span.getId());
+						String name = span.getName() == null ? null : span.getName();
+						ps.setString(3, name);
+						String service = annotation.getHost().getService_name() == null
+								? null : annotation.getHost().getService_name();
+						ps.setString(4, service);
+						ps.setString(5, annotation.getValue());
+						ps.setInt(6, annotation.getHost().getIpv4());
+						ps.setInt(7, annotation.getHost().getPort());
+						ps.setLong(8, annotation.getTimestamp());
+					});
+			if (annotation.getTimestamp()<ts) {
+				ts = annotation.getTimestamp();
+			}
+		}
+		final long createdTs = ts;
 		if (this.jdbcTemplate.queryForObject(
 				"SELECT COUNT(*) FROM zipkin_spans where span_id={id}", Integer.class,
 				span.getId()) == 0) {
@@ -42,24 +64,7 @@ public class ZipkinCollectorLauncherApplication {
 						}
 						String name = span.getName() == null ? null : span.getName();
 						ps.setString(3, name);
-						ps.setLong(5, System.currentTimeMillis());
-					});
-		}
-		for (Annotation annotation : span.getAnnotations()) {
-			this.jdbcTemplate.update(
-					"INSERT INTO zipkin_annotations (trace_id,span_id,span_name,service_name,value,ipv4,port,a_timestamp) values (?,?,?,?,?,?,?,?)",
-					(PreparedStatement ps) -> {
-						ps.setLong(1, span.getTrace_id());
-						ps.setLong(2, span.getId());
-						String name = span.getName() == null ? null : span.getName();
-						ps.setString(3, name);
-						String service = annotation.getHost().getService_name() == null
-								? null : annotation.getHost().getService_name();
-						ps.setString(4, service);
-						ps.setString(5, annotation.getValue());
-						ps.setInt(6, annotation.getHost().getIpv4());
-						ps.setInt(7, annotation.getHost().getPort());
-						ps.setLong(8, annotation.getTimestamp());
+						ps.setLong(5, createdTs);
 					});
 		}
 		for (BinaryAnnotation annotation : span.getBinary_annotations()) {
@@ -78,7 +83,7 @@ public class ZipkinCollectorLauncherApplication {
 						ps.setInt(7, annotation.getAnnotation_type().getValue());
 						ps.setInt(8, annotation.getHost().getIpv4());
 						ps.setInt(9, annotation.getHost().getPort());
-						ps.setLong(10, System.currentTimeMillis());
+						ps.setLong(10, createdTs);
 					});
 		}
 	}
